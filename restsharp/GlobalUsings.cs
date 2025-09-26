601f3b6aee3b5f44964d1984c5395d41b1fc45e6
reza <mr.dastouri@gmail.com>
2025-09-26 19:35:26 +0200

    #49 add RestSharp demo

---
+++ restsharp/GlobalUsings.cs
@@ -0,0 +1,16 @@
+global using Ivy;
+global using Ivy.Apps;
+global using Ivy.Chrome;
+global using Ivy.Core;
+global using Ivy.Shared;
+global using Ivy.Views;
+global using Ivy.Widgets.Inputs;
+global using System.Globalization;
+global using System.Collections.Immutable;
+global using System.Reactive.Linq;
+global using System.Text.Json;
+global using System.Net;
+// Note: This demo uses a simplified RestSharp for educational purposes
+global using RestSharp;
+
+namespace RestsharpDemo;
